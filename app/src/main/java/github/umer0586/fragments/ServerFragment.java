fc8366130160bf0e8d2499f5b9f2b9800a0a5ea2
umer0586 <35717992+umer0586@users.noreply.github.com>
2022-11-15 22:32:35 +0500

    ServerFragment updated Major change made to this fragment

--- app/src/main/java/github/umer0586/fragments/ServerFragment.java
+++ app/src/main/java/github/umer0586/fragments/ServerFragment.java
@@ -1,41 +1,48 @@
 package github.umer0586.fragments;
 
-import android.content.SharedPreferences;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.github.ybq.android.spinkit.SpinKitView;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.io.IOException;
 import java.net.BindException;
-import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
 import github.umer0586.R;
-import github.umer0586.sensorserver.ConnectionInfo;
-import github.umer0586.sensorserver.ConnectionInfoListener;
-import github.umer0586.sensorserver.OnServerStartListener;
-import github.umer0586.sensorserver.OnServerStopppedListener;
 import github.umer0586.sensorserver.SensorWebSocketServer;
-import github.umer0586.util.IpUtil;
+import github.umer0586.sensorserver.ServerErrorListener;
+import github.umer0586.sensorserver.ServerStartListener;
+import github.umer0586.sensorserver.ServerStopListener;
+import github.umer0586.service.SensorService;
+import github.umer0586.service.ServiceBindHelper;
+import github.umer0586.util.UIUtil;
 
 
-public class ServerFragment extends Fragment implements OnServerStartListener, OnServerStopppedListener, ConnectionsFragment.onConnectionItemClickedListener {
-
-    private SensorWebSocketServer server;
+public class ServerFragment extends Fragment
+        implements ServiceConnection, ServerStartListener, ServerStopListener, ServerErrorListener {
 
     private static final String TAG = ServerFragment.class.getSimpleName();
+
+    private SensorService sensorService;
 
     // Button at center to start/stop server
     private MaterialButton startButton;
@@ -50,11 +57,7 @@ public class ServerFragment extends Fragment implements OnServerStartListener, O
     //Ripple animation behind startButton
     private SpinKitView pulseAnimation;
 
-    private ConnectionInfoListener connectionInfoListeners;
-    private ConnectionCountListener connectionCountListener;
-
-
-    private SharedPreferences sharedPreferences;
+    private ServiceBindHelper serviceBindHelper;
 
 
     @Override
@@ -76,6 +79,12 @@ public class ServerFragment extends Fragment implements OnServerStartListener, O
         pulseAnimation = view.findViewById(R.id.loading_animation);
         cardView = view.findViewById(R.id.card_view);
 
+        serviceBindHelper = new ServiceBindHelper(
+                getContext(),
+                this,
+                SensorService.class
+        );
+
 
         hidePulseAnimation();
         hideServerAddress();
@@ -88,95 +97,36 @@ public class ServerFragment extends Fragment implements OnServerStartListener, O
                 stopServer();
         });
 
-        sharedPreferences = getContext().getSharedPreferences(getString(R.string.shared_pref_file),getContext().MODE_PRIVATE);
 
     }
 
     private void showServerAddress(final String address)
     {
-        if(server != null)
-        {
+
             cardView.setVisibility(View.VISIBLE);
             serverAddress.setVisibility(View.VISIBLE);
             serverAddress.setText(address);
-        }
 
+            showPulseAnimation();
 
     }
 
 
     private void startServer()
     {
+        Log.d(TAG, "startServer() called");
 
-        boolean localHostPref = sharedPreferences.getBoolean(getString(R.string.pref_key_localhost),false);
+        WifiManager wifiManager = (WifiManager) getContext().getApplicationContext().getSystemService(getContext().WIFI_SERVICE);
 
-        String ipAddress = null;
-
-        // is "local host" switch in enable
-        // no need to check for wifi network
-        if(localHostPref)
-            ipAddress = "127.0.0.1"; // use loopback address
-        else // check wifi
-            ipAddress = IpUtil.getWifiIpAddress(getContext());
-
-        // This condition will always be false when localHostPref is true, hence no network check
-        if(ipAddress == null)
+        if(!wifiManager.isWifiEnabled())
         {
-            Snackbar.make(getView(),"Unable to obtain Wifi IP", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(getView(),"Please enable Wi-Fi",Snackbar.LENGTH_SHORT).show();
             return;
         }
 
-        int portNo = sharedPreferences.getInt(getString(R.string.pref_key_port_no),8081);
-
-        server = new SensorWebSocketServer(
-                getContext(),
-                new InetSocketAddress(ipAddress,portNo)
-        );
-
-
-        server.setOnServerStartListener(this);
-        server.setOnServerStopped(this);
-
-        server.setConnectionInfoListener((connectionInfoArrayList)->{
-
-                if ( connectionInfoListeners != null)
-                    connectionInfoListeners.onConnectionInfo(connectionInfoArrayList);
-
-                if( connectionCountListener != null)
-                {
-                    int totalConnections = 0;
-                    for(ConnectionInfo connectionInfo : connectionInfoArrayList)
-                        totalConnections += connectionInfo.getSensorConnectionCount();
-
-                    connectionCountListener.connectionCount(totalConnections);
-
-                }
-
-        });
-
-
-            server.setOnServerError((exception)->{
-
-                getActivity().runOnUiThread(()->{
-
-                    if(exception instanceof BindException)
-                        Snackbar.make(getView(),"Port already in use",Snackbar.LENGTH_SHORT).show();
-
-                    else
-                        Snackbar.make(getView(),"Error Occurred : ",Snackbar.LENGTH_SHORT).show();
-
-                    Log.e(TAG, "onServerError() called");
-                });
-
-            });
-
-            int samplingRate = sharedPreferences.getInt(getString(R.string.pref_key_sampling_rate),200000);
-
-            server.setSamplingRate(samplingRate);
-
-            server.run();
-
-
+        serviceBindHelper.bindToService();
+        Intent intent = new Intent(getContext(), SensorService.class);
+        ContextCompat.startForegroundService(getContext(),intent);
 
     }
 
@@ -184,47 +134,113 @@ public class ServerFragment extends Fragment implements OnServerStartListener, O
 
     private void stopServer()
     {
-        if( server != null)
-        {
-            try
-            {
-                server.stop();
+        Log.d(TAG, "stopServer() called");
 
-            } catch (IOException | InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
+
+       // getContext().stopService(new Intent(getContext(),SensorService.class));
+        getContext().sendBroadcast(new Intent(SensorService.ACTION_STOP_SERVER));
 
     }
 
-    // a callback from server thread
     @Override
-    public void onServerStarted()
+    public void onPause()
     {
-        getActivity().runOnUiThread(()->{
-            showServerAddress("ws:/"+server.getAddress());
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+
+        if(sensorService != null)
+        {
+            sensorService.setServerStartListener(null);
+            sensorService.setServerStopListener(null);
+            sensorService.setServerErrorListener(null);
+        }
+
+        serviceBindHelper.unBindFromService();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+
+        serviceBindHelper.bindToService();
+
+    }
+
+    @Override
+    public void onServerStarted(String IP, int port)
+    {
+        Log.d(TAG, "onServerStarted() called");
+        UIUtil.runOnUiThread(()->{
+
+            showServerAddress("ws://"+IP+":"+port);
             showPulseAnimation();
+
             startButton.setTag("started");
             startButton.setText("STOP");
 
-            Snackbar.make(getView(),"Server started",Snackbar.LENGTH_SHORT).show();
-
+            showMessage("Server started");
         });
+
     }
 
-    // a callback from server thread
     @Override
     public void onServerStopped()
     {
-        getActivity().runOnUiThread(()->{
+        Log.d(TAG, "onServerStopped() called ");
+        UIUtil.runOnUiThread(()->{
+
             hideServerAddress();
             hidePulseAnimation();
+
             startButton.setTag("stopped");
             startButton.setText("START");
 
-            Snackbar.make(getView(),"Server stopped",Snackbar.LENGTH_SHORT).show();
+           showMessage("Server Stopped");
 
+        });
+
+    }
+
+
+    @Override
+    public void onError(Exception exception)
+    {
+
+        UIUtil.runOnUiThread(()->{
+
+            if(exception instanceof BindException)
+                showMessage("Port already in use");
+
+            else if (exception instanceof UnknownHostException)
+                showMessage("Unable to obtain IP");
+
+            else
+                showMessage("Failed to start server");
+
+            Log.w(TAG, "onServerError() called");
+
+            startButton.setTag("stopped");
+            startButton.setText("START");
+
+            hideServerAddress();
+            hidePulseAnimation();
+
+        });
+
+
+    }
+
+    // called through on onServiceConnected()
+    public void onServerAlreadyRunning(String IP, int port)
+    {
+        Log.d(TAG, "onServerAlreadyRunning() called");
+        UIUtil.runOnUiThread(()->{
+            showServerAddress("ws://"+IP+":"+port);
+            Toast.makeText(getContext(),"service running",Toast.LENGTH_SHORT).show();
+            startButton.setTag("started");
+            startButton.setText("STOP");
         });
     }
 
@@ -244,58 +260,59 @@ public class ServerFragment extends Fragment implements OnServerStartListener, O
         serverAddress.setVisibility(View.GONE);
     }
 
-    public void setConnectionInfoListener(ConnectionInfoListener listener)
-    {
-        connectionInfoListeners = listener;
-    }
-
-    public void setConnectionCountListener(ConnectionCountListener listener)
-    {
-        connectionCountListener = listener;
-    }
-
-
-    /**
-     *  A callback from ConnectionFragment when user taps connection item
-     *  we have to catch that event in this fragment because this fragment holds server object
-     */
     @Override
-    public void onConnectionItemClicked(ConnectionInfo connectionInfo)
+    public void onServiceConnected(ComponentName name, IBinder service)
     {
-        String message = "Do you want to close this connection\n" +
-                          "Sensor : " + connectionInfo.getSensor().getName() + "\n" +
-                          "Connections : " + connectionInfo.getSensorConnectionCount() + "\n\n";
+        serviceBindHelper.setBounded(true);
+        Log.d(TAG, "onServiceConnected()");
 
-        for(InetSocketAddress inetSocketAddress : connectionInfo.getConnectedClients())
-            message += inetSocketAddress + "\n";
+        SensorService.LocalBinder localBinder = (SensorService.LocalBinder) service;
+        sensorService =  localBinder.getService();
 
-        new MaterialAlertDialogBuilder(getContext())
-                .setTitle("Close Connection")
-                .setMessage(message)
-                .setPositiveButton("Yes",(dialogInterface, i) -> {
 
-                    if(server != null)
-                        server.closeConnectionBySensor(connectionInfo.getSensor());
-                    dialogInterface.dismiss();
-                })
-                .setNegativeButton("No", (dialogInterface, i) -> {
-                    dialogInterface.dismiss();
-                })
-                .setCancelable(false)
-                .show();
+
+        Log.d(TAG, "service instance : " + service);
+
+        if(sensorService != null)
+        {
+
+            sensorService.setServerStartListener(this);
+            sensorService.setServerStopListener(this);
+            sensorService.setServerErrorListener(this);
+
+            SensorWebSocketServer  sensorWebSocketServer = sensorService.getSensorWebSocketServer();
+
+            if(sensorWebSocketServer != null)
+                if(sensorWebSocketServer.isRunning())
+                    onServerAlreadyRunning(sensorWebSocketServer.getAddress().getHostName(),sensorWebSocketServer.getPort());
+
+
+
+        }
+
+
+
+
     }
-
 
     @Override
-    public void onDestroy()
+    public void onServiceDisconnected(ComponentName name)
     {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy: ");
-        stopServer();
-
+        Log.d(TAG, "onServiceDisconnected() called with: name = [" + name + "]");
+        serviceBindHelper.setBounded(false);
     }
 
-    public interface ConnectionCountListener{
-        void connectionCount(int count);
+
+    private void showMessage(String message)
+    {
+        View view = getView();
+        Context context = getContext();
+        if(view != null)
+          Snackbar.make(view,message,Snackbar.LENGTH_SHORT).show();
+        else if( context != null)
+           Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+        else
+            Log.e(TAG, "showMessage() cannot display message as getView() or getContext() returned null" );
     }
+
 }
