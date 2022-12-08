f3790aba672ea043fc15b54c49cc465a451730c5
umer0586 <35717992+umer0586@users.noreply.github.com>
2022-12-08 21:18:37 +0500

    WifiUtil.java added

---
+++ app/src/main/java/github/umer0586/util/WifiUtil.java
@@ -0,0 +1,46 @@
+package github.umer0586.util;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class WifiUtil {
+
+    public static boolean isHotspotEnabled(Context context)
+    {
+        WifiManager wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        Method[] wmMethods = wifi.getClass().getDeclaredMethods();
+
+        //is wifi access point (hotspot) enabled
+        boolean isWifiAPenabled = false;
+
+        for (Method method : wmMethods)
+        {
+            if (method.getName().equals("isWifiApEnabled"))
+            {
+
+                try
+                {
+                    method.setAccessible(true); //in the case of visibility change in future APIs
+                    isWifiAPenabled = (boolean) method.invoke(wifi);
+
+                } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return isWifiAPenabled;
+    }
+
+    public static boolean isWifiEnabled(Context context)
+    {
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        return wifiManager.isWifiEnabled();
+    }
+
+
+
+}
