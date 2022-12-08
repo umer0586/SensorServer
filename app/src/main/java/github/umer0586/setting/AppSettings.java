a18ddaa10e312dd63310930cd8bcd38b5383471c
umer0586 <35717992+umer0586@users.noreply.github.com>
2022-12-08 21:22:59 +0500

    AppSettings.java Updated

--- app/src/main/java/github/umer0586/setting/AppSettings.java
+++ app/src/main/java/github/umer0586/setting/AppSettings.java
@@ -14,8 +14,15 @@ public class AppSettings {
 
     public AppSettings(Context context)
     {
-        this.context = context;
+        this.context = context.getApplicationContext();
         sharedPreferences = context.getSharedPreferences(context.getString(R.string.shared_pref_file),context.MODE_PRIVATE);
+    }
+
+    public void savePortNo(int portNo)
+    {
+        sharedPreferences.edit()
+                .putInt(context.getString(R.string.pref_key_port_no),portNo)
+                .commit();
     }
 
     public int getPortNo()
@@ -23,14 +30,40 @@ public class AppSettings {
         return sharedPreferences.getInt(context.getString(R.string.pref_key_port_no),8081);
     }
 
+    public void saveSamplingRate(int samplingRate)
+    {
+        sharedPreferences.edit()
+                .putInt(context.getString(R.string.pref_key_sampling_rate),samplingRate)
+                .commit();
+    }
+
     public int getSamplingRate()
     {
         return sharedPreferences.getInt(context.getString(R.string.pref_key_sampling_rate),200000);
     }
 
+    public void enableLocalHostOption(boolean state)
+    {
+        sharedPreferences.edit()
+                .putBoolean(context.getString(R.string.pref_key_localhost),state)
+                .commit();
+    }
+
     public boolean isLocalHostOptionEnable()
     {
         return sharedPreferences.getBoolean(context.getString(R.string.pref_key_localhost),false);
+    }
+
+    public void enableHotspotOption(boolean state)
+    {
+        sharedPreferences.edit()
+                .putBoolean(context.getString(R.string.pref_key_hotspot), state)
+                .commit();
+    }
+
+    public boolean isHotspotOptionEnabled()
+    {
+        return sharedPreferences.getBoolean(context.getString(R.string.pref_key_hotspot), false);
     }
 
 
