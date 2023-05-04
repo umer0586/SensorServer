5b76f348ab1496aa8fa233b1045d60ebb310c0ad
umer0586 <35717992+umer0586@users.noreply.github.com>
2023-05-04 15:28:48 +0500

    ConnectionInfo.java and ConnectionCountChangeListener.java removed

--- app/src/main/java/github/umer0586/sensorserver/ConnectionInfoChangeListener.java
+++
@@ -1,8 +0,0 @@
-package github.umer0586.sensorserver;
-
-import java.util.ArrayList;
-
-@FunctionalInterface
-public interface ConnectionInfoChangeListener {
-    void onConnectionInfoChanged(ArrayList<ConnectionInfo> connectionInfos);
-}
