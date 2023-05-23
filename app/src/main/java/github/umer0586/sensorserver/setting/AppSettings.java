e5d7b3e04663a3e5ebabc02078d95290b6e82474
umer0586 <35717992+umer0586@users.noreply.github.com>
2023-05-23 15:35:00 +0500

    Code refactoring

--- app/src/main/java/github/umer0586/sensorserver/setting/AppSettings.java
+++ app/src/main/java/github/umer0586/sensorserver/setting/AppSettings.java
@@ -11,6 +11,9 @@ public class AppSettings {
     private Context context;
     private SharedPreferences sharedPreferences;
 
+    private static final int DEFAULT_PORT_NO = 8080;
+    private static final int DEFAULT_SAMPLING_RATE = 200000;
+
 
     public AppSettings(Context context)
     {
@@ -27,7 +30,7 @@ public class AppSettings {
 
     public int getPortNo()
     {
-        return sharedPreferences.getInt(context.getString(R.string.pref_key_port_no),8081);
+        return sharedPreferences.getInt(context.getString(R.string.pref_key_port_no),DEFAULT_PORT_NO);
     }
 
     public void saveSamplingRate(int samplingRate)
@@ -39,7 +42,7 @@ public class AppSettings {
 
     public int getSamplingRate()
     {
-        return sharedPreferences.getInt(context.getString(R.string.pref_key_sampling_rate),200000);
+        return sharedPreferences.getInt(context.getString(R.string.pref_key_sampling_rate),DEFAULT_SAMPLING_RATE);
     }
 
     public void enableLocalHostOption(boolean state)
