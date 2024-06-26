85a81129fced79a0a28ee6df1ee04979993c94d5
umer0586 <umerfarooq.phone@gmail.com>
2024-06-26 08:07:41 +0500

    Http server created HttpServer.kt, HttpService.kt and RequestHandler.kt are added

---
+++ app/src/main/java/github/umer0586/sensorserver/webserver/RequestHandler.kt
@@ -0,0 +1,42 @@
+package github.umer0586.sensorserver.webserver
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import com.yanzhenjie.andserver.annotation.GetMapping
+import com.yanzhenjie.andserver.annotation.RestController
+import com.yanzhenjie.andserver.http.HttpResponse
+import github.umer0586.sensorserver.setting.AppSettings
+import github.umer0586.sensorserver.util.JsonUtil
+
+@RestController
+class RequestController {
+
+    @GetMapping("/wsport")
+    fun getWebsocketPortNo(context : Context) : String {
+
+        val settings = AppSettings(context)
+        return JsonUtil.toJSON(
+                mapOf("portNo" to settings.getPortNo())
+        )
+
+    }
+
+    @GetMapping("/sensors")
+    fun getSensors(context: Context, response: HttpResponse) : String {
+
+        val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val availableSensors: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL).filter{ it.reportingMode != Sensor.REPORTING_MODE_ONE_SHOT}
+
+        return JsonUtil.toJSON(
+                availableSensors.map { sensor ->
+                    val map = mutableMapOf<String,Any>()
+                    map["name"] = sensor.name
+                    map["type"] = sensor.stringType
+                    return@map map
+                }
+        )
+
+    }
+
+}
