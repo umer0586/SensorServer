29c4f90e731d6b14c100e8dce47432d9c43cf2be
umer0586 <umerfarooq.phone@gmail.com>
2024-08-06 16:02:38 +0500

    Refactoring

--- sensors_dashboard/lib/view_model/sensors_screen_viewmodel.dart
+++ sensors_dashboard/lib/view_model/sensors_screen_viewmodel.dart
@@ -5,21 +5,8 @@ import 'package:sensors_dashboard/model/repository/sensors_repository.dart';
 
 class SensorsScreenViewmodel with ChangeNotifier{
 
-  List<Sensor> _sensors = [];
-  List<Sensor> get sensors => _sensors;
-
   final SensorsRepository sensorsRepository;
   SensorsScreenViewmodel({required this.sensorsRepository});
-
-  void fetchSensors(){
-
-    if(kReleaseMode) {
-      sensorsRepository.getSensors().then((sensors) {
-        _sensors = sensors;
-      });
-    }
-
-  }
 
   Future<List<Sensor>> getSensors() => sensorsRepository.getSensors();
  
